b968237852c50f8c7fda391975d05d8fafbd512a
lerbs <12850522+lgardner462@users.noreply.github.com>
2025-05-19 19:02:17 -0400

    changed to 2204

--- tfo/provider.tf
+++ tfo/provider.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 2.0"
     }
   }
+  cloud { 
+    
+    organization = "mrbonuts" 
+
+    workspaces { 
+      name = "mrbonuts-cli" 
+    } 
+  } 
 }
 #declare variables but leave blank so they aren't hard coded
 variable "do_token" {}
--- tfo/www-1.tf
+++ tfo/www-1.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_droplet" "www-1" {
-    image = "ubuntu-20-04-x64"
+    image = "ubuntu-22-04-x64"
     name = "www-1"
     region = "nyc3"
     size = "s-1vcpu-1gb"
