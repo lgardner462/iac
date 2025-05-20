0a5838f9d47a220c80fd77f886de38710055d9c8
lerbs <12850522+lgardner462@users.noreply.github.com>
2025-05-20 05:06:52 -0400

    try automation

--- tfo/www-1.tf
+++ tfo/www-1.tf
@@ -10,7 +10,7 @@ connection {
     host = self.ipv4_address
     user = "root"
     type = "ssh"
-    private_key = file(var.pvt_key)
+    private_key = var.pvt_key
     timeout = "2m"
   }
 provisioner "remote-exec" {
