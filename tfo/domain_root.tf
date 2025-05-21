4738a6e8fe85d15328edef473e60e9b89b9ae9bd
lerbs <12850522+lgardner462@users.noreply.github.com>
2025-05-21 09:44:55 -0400

    added in second tfo server and rearranged domains

--- tfo/domain_root.tf
+++ tfo/domain_root.tf
@@ -1,11 +1,12 @@
 resource "digitalocean_domain" "my_domain" {
    name = var.my_host
+   ip_address = digitalocean_droplet.www-1.ipv4_address
 }
 resource "digitalocean_record" "web" {
   domain = digitalocean_domain.my_domain.id
   type   = "A"
   name   = "web"
-  value  = digitalocean_droplet.www-1.ipv4_address  # Auto-updates with Droplet IP
+  value  = digitalocean_droplet.www-2.ipv4_address  # Auto-updates with Droplet IP
 
 
 }
