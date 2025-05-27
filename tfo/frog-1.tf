dcacb02782fdb3bb92e1b5762f4bbb4d07a97c56
lerbs <12850522+lgardner462@users.noreply.github.com>
2025-05-27 08:25:09 -0400

    added a wait

--- tfo/frog-1.tf
+++ tfo/frog-1.tf
@@ -22,7 +22,8 @@ provisioner "remote-exec" {
       "sudo apt install -y nginx ansible",
       "echo '${var.deploy_key}' > ~/.ssh/id_ed25519",
       #make sure dnf is available post cloud-init but pre-ansible
-      "for i in {1..10}; do sudo dnf check-update && break || sleep 10 ; done",
+#      "for i in {1..10}; do sudo dnf check-update && break || sleep 10 ; done",
+      "sleep 60"
       "chmod 600 ~/.ssh/id_ed25519",
       "/usr/bin/ansible-pull --private-key ~/.ssh/id_ed25519 --accept-host-key --verbose --url ${var.gitrepo} --directory /var/local/src/instance-bootstrap ${var.playbook}"
     ]
